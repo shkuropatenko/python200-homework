9c39985eaed266d4f870ea9c7f2ff2923463fc50
DmytroShk <dmytro.shkuropatenko@jelvix.com>
2026-04-05 17:42:03 -0400

    Q3, Q4, Q5

--- assignments_01/warmup_01.py
+++ assignments_01/warmup_01.py
@@ -28,4 +28,22 @@ print(df.dtypes)
 
 # Pandas Q2
 print("\n" + "=" * 40)
+print("Filter the rows to show only students who passed and have a grade above 80")
 print(df[(df["grade"] > 80) & (df["passed"] == True)])
+
+# Pandas Q3
+print("\n" + "=" * 40)
+print("Add a new column called 'grade_curved' that Adds 5 points to each student's grade")
+df["grade_curved"] = df["grade"] + 5
+print(df)
+
+# Pandas Q4
+print("\n" + "=" * 40)
+print("name in uppercase")
+df["name_upper"] = df["name"].str.upper().str.strip()
+print(df[["name", "name_upper"]].head())
+
+# Pandas Q5
+print("\n" + "=" * 40)
+print("compute the mean grade for each city")
+print(df.groupby("city")["grade"].mean())
