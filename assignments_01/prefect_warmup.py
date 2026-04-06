bc39c281f0eead656144d86b410b9a87f8aa0f9d
DmytroShk <dmytro.shkuropatenko@jelvix.com>
2026-04-06 16:28:56 -0400

    Added clean_data func

--- assignments_01/prefect_warmup.py
+++ assignments_01/prefect_warmup.py
@@ -4,7 +4,17 @@ import pandas as pd
 arr = np.array([12.0, 15.0, np.nan, 14.0, 10.0, np.nan, 18.0, 14.0, 16.0, 22.0, np.nan, 13.0])
 
 def create_series(arr): 
-  return pd.Series(arr, name="values")
+  created_s = pd.Series(arr, name="values")
+  return created_s
 
-create_series(arr)
-print(create_series(arr))
+created_series = create_series(arr)
+
+# print(created_series)
+
+def clean_data(created_series):
+  clean_s = created_series.dropna().reset_index(drop=True)
+  return clean_s
+
+cleaned_data = clean_data(created_series)
+
+print(cleaned_data)
