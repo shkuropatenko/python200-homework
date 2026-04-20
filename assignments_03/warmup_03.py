6446f3b8a7aa0ea0bf93b05e9fd15553834a22fc
DmytroShk <dmytro.shkuropatenko@jelvix.com>
2026-04-19 21:28:04 -0400

    add preprocessing section

--- assignments_03/warmup_03.py
+++ assignments_03/warmup_03.py
@@ -18,3 +18,23 @@ from sklearn.metrics import (
 iris = load_iris(as_frame=True)
 X = iris.data
 y = iris.target
+
+# --- Preprocessing ---
+# Q1
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, stratify=y, random_state=42
+)
+
+print("X_train shape:", X_train.shape)
+print("X_test shape:", X_test.shape)
+print("y_train shape:", y_train.shape)
+print("y_test shape:", y_test.shape)
+
+# Q2
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+print("X_train_scaled means:", np.mean(X_train_scaled, axis=0))
+
+# We fit the scaler on X_train only to avoid leaking information from the test set.
