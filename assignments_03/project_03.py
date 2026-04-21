cbc513bfc581e9c7e4ab40489feef70e1b5b78ab
DmytroShk <dmytro.shkuropatenko@jelvix.com>
2026-04-20 23:44:45 -0400

    apply scaling to features

--- assignments_03/project_03.py
+++ assignments_03/project_03.py
@@ -36,3 +36,9 @@ for f in features:
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42, stratify=y
 )
+
+scaler = StandardScaler()
+
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+# scaling is important because features have very different ranges
